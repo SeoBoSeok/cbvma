a9f9089030ef345902360e6845ca6e8507a40a96
SeoBoSeok <ggy7bbo@gmail.com>
2020-10-15 15:15:14 +0900

    hot fix cache

--- head.sub.php
+++ head.sub.php
@@ -41,6 +41,9 @@ header("Pragma: no-cache"); // HTTP/1.0
 <html lang="ko">
 <head>
 <meta charset="utf-8">
+<meta http-equiv="Expires" content="-1" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Cache-Control" content="no-cache" />
 <?php
 if (G5_IS_MOBILE) {
     echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10,user-scalable=yes">'.PHP_EOL;
